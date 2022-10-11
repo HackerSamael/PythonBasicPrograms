49f700efbab0a8b84469d62aa8fa24af49bbb909
arshaann <115551083+arshaann@users.noreply.github.com>
2022-10-11 17:51:08 +0530

    added a new file

---
+++ BubbleSort.py
@@ -0,0 +1,15 @@
+A = []
+n = int(input("Enter the number of elemnts: "))
+for i in range (n):
+	element = int(input("Enter the element: "))
+	A.append(element)    
+l = len(A)
+i = 0
+print(f"Old list Was {A}")
+while i!=l:
+	N = l-i-1
+	for j in range(N):
+		if A[j]>=A[j+1]:
+			A[j],A[j+1]=A[j+1],A[j]
+	i+=1
+print(f"New list is {A}")
